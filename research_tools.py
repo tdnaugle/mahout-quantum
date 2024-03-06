2df87462f9eddf8fbfc34b3be48f5273195f3cde
manifold <tdnaugle@gmail.com>
2024-03-05 22:59:43 -0600

    indent on docstring

--- research_tools.py
+++ research_tools.py
@@ -2,18 +2,17 @@ import functools
 import sys
 
 def trace_calls_with_filter(filter_str):
-"""
-Example usage of printing the qiskit-only calls from a qiskit operation:
+    """
+    Example usage of printing the qiskit-only calls from a qiskit operation:
 
-@trace_calls_with_filter("qiskit")
-def trace_state_vector():
-    qiskit_basis_state = Statevector.from_label("01")
+    @trace_calls_with_filter("qiskit")
+    def trace_state_vector():
+        qiskit_basis_state = Statevector.from_label("01")
 
 
-if __name__ == "__main__":
-    trace_state_vector()
-
-"""
+    if __name__ == "__main__":
+        trace_state_vector()
+    """
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
